ffd8a8e7ad3614004738b71a2731341617e87e2e
Tiffany Carter <tafby88@gmail.com>
2019-01-23 20:29:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,8 @@
 def game_hash 
   hashketball = {
     :home => {
-      :team_name => "Brookyn Nets"
-      :colors => ["Black", "White"]
+      :team_name => "Brookyn Nets",
+      :colors => ["Black", "White"],
       :players => {
         "Alan Anderson" => {
           :number => 0,
